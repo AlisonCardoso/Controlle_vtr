7443aae8af148765e20c7c772cafb207e0482ea3
AlisonCardoso <88635010+AlisonCardoso@users.noreply.github.com>
2024-05-24 19:21:23 -0300

    traducao da autenticação

--- controle_wev_viaturas/resources/views/auth/register.blade.php
+++ controle_wev_viaturas/resources/views/auth/register.blade.php
@@ -8,6 +8,7 @@
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
+    
 
         <!-- Email Address -->
         <div class="mt-4">
@@ -16,19 +17,9 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+       
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- usuario -->
+        <!-- user-->
         <div class="mt-4">
             <x-input-label for="username" :value="__('Username')" />
 
@@ -58,7 +49,17 @@
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
+ <!-- Password -->
+ <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
 
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
 
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
